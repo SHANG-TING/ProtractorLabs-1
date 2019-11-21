96bc4325662c4774702b80a3f9fb6fef04470bd5
Neil <rebom8888@gmail.com>
2019-11-21 15:50:21 +0800

    實戰演練：練習表單操作

--- e2e/src/events/create-event.e2e-spec.ts
+++ e2e/src/events/create-event.e2e-spec.ts
@@ -1,3 +1,77 @@
-describe('add a new event', () => {
+import * as path from 'path';
+import {
+  browser,
+  by,
+  element,
+  $,
+  $$,
+  ExpectedConditions as EC
+} from 'protractor';
 
+// • 測試案例
+//   – 輸入活動名稱 Protractor 實戰
+//   – 使用 Datepicker 選擇活動日期 2019/11/16
+//     • 請勿使用 sendKeys 測試
+//     • 進階練習：請嘗試設定 1997/12/31 這個日期！
+//   – 輸入活動時間 早上；輸入活動票價 500
+//   – 輸入活動地址 中正路100號；城市 台北市；國家 台灣
+//   – 輸入活動網址 http://example.com
+//   – 上傳活動圖片 e2e\src\assets\Protractor.png
+//   – 點擊儲存按鈕，驗證活動列表顯示 Protractor 實戰 活動
+
+describe('add a new event', () => {
+  beforeAll(async () => {
+    await browser.get('/events/new');
+  });
+
+  it('should 驗證活動列表顯示 Protractor 實戰 活動', async () => {
+    await $('#name').sendKeys('Protractor 實戰');
+
+    //#region 設定 1997/12/31 這個日期
+    {
+      await $('mat-datepicker-toggle .mat-icon-button').click();
+
+      await $('.mat-calendar-period-button').click();
+
+      await $('.mat-calendar-previous-button').click();
+
+      await element(
+        by.cssContainingText('.mat-calendar-body-cell-content', '1997')
+      ).click();
+
+      await element(
+        by.cssContainingText('.mat-calendar-body-cell', 'DEC')
+      ).click();
+
+      await element(
+        by.cssContainingText('.mat-calendar-body-cell-content', '31')
+      ).click();
+
+      expect(await $('#eventDate').getAttribute('value')).toBe('12/31/1997');
+    }
+    //#endregion
+
+    await $('#eventTime').sendKeys('早上');
+    await $('#eventPrice').sendKeys('500');
+    await $('#address').sendKeys('中正路100號');
+    await $('#city').sendKeys('台北市');
+    await $('#country').sendKeys('台灣');
+    await $('#onlineUrl').sendKeys('http://example.com');
+    await $('#imageFile').sendKeys(
+      path.resolve('./e2e/src/assets/Protractor.png')
+    );
+
+    await element(by.buttonText('儲存')).click();
+
+    expect(await browser.getCurrentUrl()).toContain('/events');
+
+    expect(
+      element(
+        by.cssContainingText(
+          'event-thumbnail h2',
+          'Protractor 實戰'.toUpperCase()
+        )
+      )
+    ).toBeDefined();
+  });
 });
--- e2e/src/events/create-session.e2e-spec.ts
+++ e2e/src/events/create-session.e2e-spec.ts
@@ -1,3 +1,71 @@
-describe('add a new session', () => {
+import {
+  browser,
+  by,
+  element,
+  $,
+  $$,
+  ExpectedConditions as EC
+} from 'protractor';
 
+// • 測試案例
+//   – 點擊 建立議程
+//   – 輸入議程名稱 Protractor 表單練習
+//   – 輸入主講人 John
+//   – 選擇演講時間 一小時
+//   – 選擇場次 上午場；選擇適合程度 初級、中級
+//   – 輸入演講內容 自動化 Protractor 表單
+//   – 點擊儲存按鈕，驗證議程列表顯示 Protractor 表單練習
+
+describe('add a new session', () => {
+  beforeAll(async () => {
+    await await browser.get('/events/4');
+  });
+
+  it('should 驗證議程列表顯示 Protractor 表單練習', async () => {
+    await element(by.linkText('建立議程')).click();
+
+    await $('#sessionName').sendKeys('Protractor 表單練習');
+    await $('#presenter').sendKeys('John');
+
+    //#region 選擇演講時間 一小時
+    {
+      const durationList = $('select[name=duration]');
+
+      await durationList
+        .element(by.cssContainingText('option', '一小時'))
+        .click();
+
+      expect(await durationList.getAttribute('value')).toBe('2');
+    }
+    //#endregion
+
+    //#region 選擇場次 上午場
+    {
+      const radMorning = $('input[type=radio][name=period][value=上午場]');
+
+      await radMorning.click();
+
+      expect(await radMorning.getAttribute('checked')).toBeTruthy();
+    }
+    //#endregion
+
+    //#region 選擇適合程度 初級、中級
+    {
+      const chk1 = $('input[type=checkbox][name=level][value=初級]');
+      const chk2 = $('input[type=checkbox][name=level][value=中級]');
+
+      await chk1.click();
+      await chk2.click();
+
+      expect(await chk1.getAttribute('checked')).toBeTruthy();
+      expect(await chk2.getAttribute('checked')).toBeTruthy();
+    }
+    //#endregion
+
+    await $('#abstract').sendKeys('自動化 Protractor 表單');
+
+    await element(by.buttonText('儲存')).click();
+
+    expect(await $('collapsible-well h4').getText()).toBe('Protractor 表單練習');
+  });
 });
